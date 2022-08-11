4f80de1475f47af322980d30173a39369a21def4
Ashe Kim <ashekim@robotis.com>
2022-08-12 08:09:12 +0900

    init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src
+# Build directory: /home/ubuntu/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("turtlebot3/turtlebot3")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_2020")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_core")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_msgs")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_driving")
+subdirs("ld08_driver")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_camera")
+subdirs("turtlebot3_autorace_2020/turtlebot3_autorace_detect")
+subdirs("turtlebot3/turtlebot3_bringup")
